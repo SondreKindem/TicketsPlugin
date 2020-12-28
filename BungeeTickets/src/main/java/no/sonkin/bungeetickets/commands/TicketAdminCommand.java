9aa9578799391c09c6b580f9e744b980f31e9802
Sondre Kindem <sondre.kindem@gmail.com>
2020-12-28 15:54:44 +0100

    feat: implement initial ticket close

--- BungeeTickets/src/main/java/no/sonkin/bungeetickets/commands/TicketAdminCommand.java
+++ BungeeTickets/src/main/java/no/sonkin/bungeetickets/commands/TicketAdminCommand.java
@@ -4,17 +4,29 @@ import co.aikar.commands.BaseCommand;
 import co.aikar.commands.CommandHelp;
 import co.aikar.commands.annotation.*;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
+import no.sonkin.bungeetickets.BungeeTickets;
+import no.sonkin.ticketscore.exceptions.TicketException;
 
 @CommandAlias("ticketadmin|ta")
 @CommandPermission("tickets.admin")
 public class TicketAdminCommand extends BaseCommand {
 
     @Subcommand("close")
-    @Syntax("<id> <reason>")
-    @CommandCompletion("<id> <reason>")
+    @Syntax("<id>")
+    @CommandCompletion("<id>")
     @Description("Close a ticket")
     public static void close(CommandSender sender, String[] args) {
-
+        try {
+            if (args.length > 0) {
+                int id = Integer.parseInt(args[0]);
+                BungeeTickets.getInstance().getTicketsCore().getTicketManager().closeTicket(id);
+            }
+        } catch (TicketException e) {
+            sender.sendMessage(new TextComponent("§cCould not close ticket! Reason:\n" + e.getMessage()));
+        } catch (NumberFormatException e) {
+            sender.sendMessage(new TextComponent("§cNot a valid ID"));
+        }
     }
 
     @HelpCommand
