2297a63bca49210f8ad44a17ee2b8b7ee797b8e5
Sondre Kindem <sondre.kindem@gmail.com>
2020-12-29 21:39:01 +0100

    chore: minor string formatting

--- BungeeTickets/src/main/java/no/sonkin/bungeetickets/MessageBuilder.java
+++ BungeeTickets/src/main/java/no/sonkin/bungeetickets/MessageBuilder.java
@@ -9,7 +9,7 @@ import no.sonkin.ticketscore.models.Ticket;
 import java.util.Date;
 
 public class MessageBuilder {
-    public static String prefix = "[§6Tickets§r] ";
+    public static String prefix = "§r[§6Tickets§r] ";
     public static String separator = "§b--------------------------------";
 
     public static TextComponent info(String message) {
@@ -21,7 +21,7 @@ public class MessageBuilder {
     }
 
     public static BaseComponent[] ticket(Ticket ticket) {
-        return new ComponentBuilder("§b------------- " + prefix + " §b-------------")
+        return new ComponentBuilder("§b------------- " + prefix + "§b-------------")
                 .append("\n§bDisplaying ticket No. §a" + ticket.getID())
                 .append("\n§c§l- §bBy: §a" + ticket.getPlayerName())
                 .append("\n§c§l- §bon §a" + ticket.getServerName() + " §bin §a" + ticket.getWorld())
